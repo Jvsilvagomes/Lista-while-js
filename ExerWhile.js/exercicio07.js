64c3d71531576af25bc697340a5341f0b1067cc3
João Victor <joao.silvassenai@gmail.com>
2025-04-11 11:12:26 -0300

    exercicio07

---
+++ ExerWhile.js/exercicio07.js
@@ -0,0 +1,10 @@
+let dinheiro = 0;
+let moedas = 0;
+
+while(dinheiro < 12){
+    dinheiro += 3;
+    moedas++;
+    console.log(`Tem ${dinheiro} reais com ${moedas} moedas`)
+}
+
+// joao victor 11/04

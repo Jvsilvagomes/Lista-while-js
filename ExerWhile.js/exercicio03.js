66e15df416446ce11fcda03b98080bd3e7baa57d
João Victor <joao.silvassenai@gmail.com>
2025-04-11 10:33:27 -0300

    exercicio03

---
+++ ExerWhile.js/exercicio03.js
@@ -0,0 +1,8 @@
+let soma = 0;
+let contador = 0;
+while(soma <= 40){
+   contador ++;
+   soma += contador
+}
+console.log(`A soma final é ${soma}`)
+console.log (`A quantidade de numeros somados é ${contador}`)
